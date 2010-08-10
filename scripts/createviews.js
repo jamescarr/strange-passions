a3971a3ff649a19c9aabb4963bb8a6e1432e04e6
James Carr <james.r.carr@gmail.com>
2010-08-09 21:03:39 -0400

    fixed package.json, setup couchdb install script

---
+++ scripts/createviews.js
@@ -0,0 +1,26 @@
+var Connection = require('cradle').Connection,
+    fs = require('fs'),
+    sys = require('sys')
+
+var DESIGN_DOCS_DIR = __dirname + '/designdocs/'
+
+var db = new Connection().database('strangepassions')
+
+
+db.exists(function(err, result){
+  if(!result){
+    db.create();
+  }
+  fs.readdir(DESIGN_DOCS_DIR, function(err, files){
+    files.forEach(function(fileName){
+      fs.readFile(DESIGN_DOCS_DIR+fileName, function(err, data){
+        var designDocName = '_design/'+fileName.replace(/\.json$/, '')
+        db.get(designDocName, function(err, doc){
+          db[doc.error?'insert':'save'](designDocName, JSON.parse(data), function(err, res){
+            sys.puts('[DB] ' + res)
+          })
+        })
+      })
+    })
+  })
+})
