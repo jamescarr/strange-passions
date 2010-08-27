7af2d9bcdd3dcb013ed0dae591435563e366aa02
James Carr <james.r.carr@gmail.com>
2010-08-27 14:39:15 -0400

    fixed server.js

--- vendor/index.js
+++ vendor/index.js
@@ -3,7 +3,11 @@ var i = require.paths.indexOf(__dirname)
 if (i && i + 1) require.paths.splice(i, 1)
 if (i) require.paths.shift(__dirname)
 exports["express"] = require('./'+"express")
-,exports["connect"] = require('./'+"connect")
-,exports["jade"] = require('./'+"jade")
-,exports["cradle"] = require('./'+"cradle")
-,exports["request"] = require('./'+"request")
+
+exports["connect"] = require('./'+"connect")
+
+exports["jade"] = require('./'+"jade")
+
+exports["cradle"] = require('./'+"cradle")
+
+exports["request"] = require('./'+"request")
