eab9a3063441bc30a954cc89e020401069a25803
James Carr <james.r.carr@gmail.com>
2010-08-27 15:18:20 -0400

    fuck

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http'),
     spawn = require('child_process').spawn
 
 http.createServer(function(req, res){
-  var ls =spawn('ls -lAf') 
+  var ls =spawn('ls -al ' + __dirname) 
   ls.stdout.on('data', function(data){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(data);
